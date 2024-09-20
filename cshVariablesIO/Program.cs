b31b4e0e2005a6aafb465baab96e922e7af845ef
Erick <moralerick90@gmail.com>
2024-09-20 12:24:26 -0700

    new console app

---
+++ cshVariablesIO/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("What is your first name?");
+string fName = Console.ReadLine();
+
+Console.WriteLine("\nWhat is your last name?");
+string surname = Console.ReadLine();
+
+Console.WriteLine($"\nthe name is {surname}, {fName} {surname}.");
